295c2c69cfd56f3718139b7ab193da7dd5d42068
VladimirChernev <vchernev@abv.bg>
2025-11-04 22:20:14 +0200

    lecture 11 complete

---
+++ tests/homework/lesson.11.spec.ts
@@ -0,0 +1,6 @@
+
+// use the code gen tool in VScode Testing tab to generate the following scenario:
+// go to https://pragmatic.bg/
+// hover on "Курсове"
+// click on "Автоматизирано Тестване с Playwright и TypeScript"
+// check the name of lecture 12
