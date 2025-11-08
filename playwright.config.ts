7303c68f1e735d51c848eea2bb5a58a3e1a2dc6c
VillyGoutova <38793250+VillyGoutova@users.noreply.github.com>
2025-11-08 10:06:17 +0200

    homework test

--- playwright.config.ts
+++ playwright.config.ts
@@ -60,10 +60,10 @@ export default defineConfig({
     //   use: { ...devices['Desktop Chrome'] },
     // },
 
-    // {
-    //   name: 'firefox',
-    //   use: { ...devices['Desktop Firefox'] },
-    // },
+    {
+      name: 'Firefox',
+      use: { ...devices['Desktop Firefox'], channel: 'firefox' },
+    },
 
     // {
     //   name: 'webkit',
@@ -85,10 +85,10 @@ export default defineConfig({
     //   name: 'Microsoft Edge',
     //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
     // },
-    {
-      name: 'Google Chrome',
-      use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    },
+    //{
+     // name: 'Google Chrome',
+    //  use: { ...devices['Desktop Chrome'], channel: 'chrome' },
+    //},
   ],
 
   /* Run your local dev server before starting the tests */
--- tests/homework/lesson.11.spec.ts
+++ tests/homework/lesson.11.spec.ts
@@ -6,3 +6,20 @@
 // check the name of lecture 12
 
 // afterwords you will have a new file in project/tests folder. Copy its contents into this file and remove the old one.
+
+import { test, expect } from '@playwright/test';
+
+test('Check lecture 12 name in Playwright course', async ({ page }) => {
+  
+  await page.goto('https://pragmatic.bg/');
+  await page.getByRole('link', { name: 'Курсове' }).hover();
+  await page.locator('#menu-item-9220 > a').click();
+  await page.waitForLoadState('networkidle');
+  const lecture12h2 = page.locator('li article.dt_lessons').nth(11).locator('h2');
+  await expect(lecture12h2).toBeVisible();
+  const lecture12Name = await lecture12h2.textContent();
+  
+  console.log('Lecture 12 name:', lecture12Name?.trim());
+  
+ 
+});
