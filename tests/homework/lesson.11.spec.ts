274c4f58ba9101058c849c55107c4b7b0311ba79
Desislava Laskova <desislava.laskova@gmail.com>
2025-11-08 14:01:16 +0200

    lesson11_homework

--- tests/homework/lesson.11.spec.ts
+++ tests/homework/lesson.11.spec.ts
@@ -6,3 +6,12 @@
 // check the name of lecture 12
 
 // afterwords you will have a new file in project/tests folder. Copy its contents into this file and remove the old one.
+
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('https://pragmatic.bg/');
+  await page.getByRole('link', { name: 'Курсове', exact: true }).hover();
+  await page.locator('#menu-item-9220').getByRole('link', { name: 'Автоматизирано Тестване с' }).click();
+  await page.getByRole('link', { name: 'Основи на тестването с' }).click();
+});
