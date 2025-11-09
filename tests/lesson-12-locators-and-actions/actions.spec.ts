c6448ff7ddfd1e93b8977ee3c77271c70c8add32
VladimirChernev <vchernev@abv.bg>
2025-11-09 10:10:03 +0200

    lecture 12 finished

--- tests/lesson-12-locators-and-actions/actions.spec.ts
+++ tests/lesson-12-locators-and-actions/actions.spec.ts
@@ -4,4 +4,52 @@ import { test } from '@playwright/test';
 
 test('Actions Examples', async ({ page }) => {
   await page.goto('https://st2016.inv.bg/login');
+
+  await page.pause();
+
+  // Text Input
+  // await page.locator('#loginusername').fill('username');
+
+  // Checkbox and Radio buttons
+  await page.locator('#persistent').check();
+  await page.getByRole('radio', { name: 'English' }).check();
+
+  // Select Option from Dropdown
+  await page.locator('#searchLanguage').selectOption('af');
+
+  // Multiple Option Dropdown Selection
+  await page.locator('Choose multiple colour').selectOption(['red', 'blue', 'green'])
+
+  // Mouse clicks
+  // Generic click
+  await page.locator('#loginsubmit').click();
+
+  // Double click
+  await page.locator('.fas.fa-eye').dblclick();
+
+  // Right click
+  await page.locator('#loginusername').click({ button: 'right'});
+
+  // Shift + Click
+  await page.locator('#loginsubmit').click({ modifiers: ['Shift'] });
+
+  // Ctr + click
+  await page.locator('#loginsubmit').click({ modifiers: ['ControlOrMeta'] });
+
+  // Click at specific position (x=0 and y=0 meand top left corner)
+  await page.locator('.login-logo-container--login__img').click({ position: { x: 0, y: 0 } });
+
+  // Focus element
+  await page.locator('.selenium-forgotten-page').focus();
+
+  // Hover element
+  await page.locator('#newpass2').hover();
+
+  // Drag and drop
+  await page.locator('#newpass2').dragTo(page.locator('#loginusername'));
+
+  // Keyboard input
+  await page.locator('#loginpassword').press('Enter');
+
+  await page.pause();
 });
--- tests/lesson-12-locators-and-actions/locators.spec.ts
+++ tests/lesson-12-locators-and-actions/locators.spec.ts
@@ -5,4 +5,76 @@ import { test } from '@playwright/test';
 
 test('Locator Examples', async ({ page }) => {
   await page.goto('https://st2016.inv.bg/login');
+
+  /*
+    Locator - Selector (Xpath or CSS)
+  */
+
+  // find by Id
+  await page.locator('#loginusername').fill('Username'); // CSS
+  await page.locator('//*[@id="loginusername"]').fill('Username'); // Xpath
+
+  // find by Attribute - Name
+  await page.locator('[name="email"]').fill('Username'); // CSS
+  await page.locator('//*[@name="email"]').fill('Username'); // Xpath
+
+  // find by Attribute - tabindex
+  await page.locator('[tabindex="1"]').fill('Username'); // CSS
+  await page.locator('//*[@tabindex="1"]').fill('Username'); // Xpath
+
+  // find by Class
+  await page.locator('.textinput.selenium-login-email').fill('Username'); // CSS
+  await page.locator('//*[contains(@class,"selenium-login-email")]').fill('Username');
+
+  // find by Text
+  await page.locator('text="Забравена парола?"').click(); // CSS
+  await page.locator('//*[contains(text(),"Забравена парола?")]').click(); // Xpath
+
+  // combination Example
+  await page.locator('.operator').filter({ hasText: 'Чат с оператор' }).click();
+
+  /* 
+  Playwright Built-in Locators
+  */
+
+  // find by Role
+  await page.getByRole('button', { name: 'Вход' }).click();
+
+  // find by Text
+  await page.getByText('запомни входа от този компютър').check();
+
+  // find by Label
+  await page.getByLabel('English').check();
+  await page.getByLabel('EN').check();
+
+  // find by Test Id
+  await page.getByTestId('Chechbox test').check();
+
+  // find by Placeholder
+  await page.getByPlaceholder('Search IMDb').fill('Predator');
+
+  // find by Title
+  await page.getByTitle('English — Wikipedia — The Free Encyclopedia').click();
+
+  // find by Alt text
+  await page.getByAltText('Get Certified').click();
+
+  /*
+    Working with multiple elements
+  */
+
+  // count all elements found by locator
+  console.log(await page.locator('//*[contains(@class,"selenium")]').count());
+
+  // find all locators and print them
+  console.log((await page.locator('//*[contains(@class,"selenium")]').all()).toString());
+
+  // find first in a list of locators
+  await page.locator('//*[contains(@class,"selenium")]').first().click();
+
+  // find last in a list of locators
+  await page.locator('//*[contains(@class,"selenium")]').last().click();
+
+  // find n-th in a list of locators
+  await page.locator('//*[contains(@class,"selenium")]').nth(5).click();
 });
