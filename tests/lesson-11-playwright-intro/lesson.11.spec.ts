411473028cd33885a76cd3692a026614951752ab
Desislava Laskova <desislava.laskova@gmail.com>
2025-11-09 10:16:00 +0200

    Lecture11-Homework

--- tests/lesson-11-playwright-intro/lesson.11.spec.ts
+++ tests/lesson-11-playwright-intro/lesson.11.spec.ts
@@ -9,6 +9,16 @@
 
 import { test, expect } from '@playwright/test';
 
+// documentation: https://playwright.dev/docs/writing-tests
+
+test('Basic Scenario', async ({ page }) => {
+  await page.goto('https://playwright.dev/'); // Open a new browser, context, page tab and navigate to URL
+  await expect(page).toHaveTitle(/Playwright/); // Expect a "title" to contain a substring
+
+  await page.getByRole('link', { name: 'Get started' }).click(); // Click the Get "Started" link
+  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible(); // Expects page to have a heading with the name "Installation"
+});
+
 test('test', async ({ page }) => {
   await page.goto('https://pragmatic.bg/');
   await page.getByRole('link', { name: 'Курсове', exact: true }).hover();
