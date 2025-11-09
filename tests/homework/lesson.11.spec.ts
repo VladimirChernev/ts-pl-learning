5478768ff8de0afbc022739ccf6d1a9f4a84ae3a
TChakarova <chakarovat@yahoo.com>
2025-11-09 13:03:16 +0200

    publish teodora-chakarova branch

--- tests/homework/lesson.11.spec.ts
+++ tests/homework/lesson.11.spec.ts
@@ -1,8 +1,12 @@
+import { test, expect } from '@playwright/test';
 
-// use the code gen tool in VScode Testing tab to generate the following scenario:
-// go to https://pragmatic.bg/
-// hover on "Курсове"
-// click on "Автоматизирано Тестване с Playwright и TypeScript"
-// check the name of lecture 12
+test('navigate to pragmatic.bg and verify lecture 12', async ({ page }) => {
+  await page.goto('https://pragmatic.bg/');
 
-// afterwords you will have a new file in project/tests folder. Copy its contents into this file and remove the old one.
+  await page.getByRole('link', { name: 'Курсове', exact: true }).hover();
+  await page.locator('#menu-item-9220').getByRole('link', { name: 'Автоматизирано Тестване с' }).click();
+
+  const lectureName = await page.getByRole('link', { name: 'Основи на тестването с' }).textContent();
+  expect(lectureName).toContain('Основи на тестването с');
+  await page.close();
+});
