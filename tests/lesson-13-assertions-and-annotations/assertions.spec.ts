2ed380043cfdabc18aadf9e14fd1c53f8f8c0375
VladimirChernev <vchernev@abv.bg>
2025-11-12 00:15:07 +0200

    updates assertions

--- tests/lesson-13-assertions-and-annotations/assertions.spec.ts
+++ tests/lesson-13-assertions-and-annotations/assertions.spec.ts
@@ -4,81 +4,89 @@ import { test, expect } from '@playwright/test';
 // playwright documentation: https://playwright.dev/docs/test-assertions
 
 test('Assertions Examples', async ({ page }) => {
-  await page.goto('https://st2016.inv.bg/');
+  await test.step('Navigate to Landing Page', async () => {
+    await page.goto('https://st2016.inv.bg/');
+    await expect(page).toHaveTitle('Вход - QA Ground');
+  });
 
-  await page.locator('#loginusername').fill('karamfilovs@gmail.com');
-  await page.locator('#loginpassword').fill('111111');
-
-  await page.locator('#loginsubmit').click();
-  await page.getByRole('link', { name: 'Нова Фактура', exact: true }).click();
+  await test.step('Login', async () => {
+    await page.locator('#loginusername').fill('karamfilovs@gmail.com');
+    await page.locator('#loginpassword').fill('111111');
+    await page.locator('#loginsubmit').click();
+    await page.getByRole('link', { name: 'Нова Фактура', exact: true }).click();
+  });
 
   /* 
-    Non-retrying (Synchronous) assertions
+    Non-retrying (Synchronous) assertions:
   */
-
   expect(true).toBeTruthy();
   expect(false).toBeFalsy();
   expect(1).toEqual(1);
   expect('success').toBe('success');
   expect('Playwright').toContain('Play');
 
-  // Polling
-  expect
+  // Polling turns a non-retrying assertrion into a retrying one:
+  await expect
     .poll(
       async () => {
-        return 'success';
+        return true;
       },
       {
-        timeout: 5000,
-        intervals: [1500, 3000, 4500],
+        timeout: 6000, // Custom timeout, Care!: 0 here will wait forever!
+        intervals: [1500, 3000, 4500], // Custom intervals for retries
       },
     )
-    .toBe('success');
+    .toBeTruthy();
 
-  /*
-    Auto-retrying (Web-First) assertions
+  /* 
+    Auto-retrying (Web-First) assertions:
   */
 
-  // Custom Timeout:
+  // Custom timeout
   await expect(page.getByRole('heading', { name: 'Нова фактура' })).toBeVisible({ timeout: 7000 });
 
-  // check element visibility
+  // element visibility:
   await expect(page.getByRole('heading', { name: 'Нова фактура' })).toBeVisible();
   await expect(page.locator('#firmPriceRounding')).toBeHidden();
 
-  // check elemt text
-  await expect(page.locator('#headline2')).toHaveText('нова фактура', { ignoreCase: true });
-  await expect(page.locator('#headline2')).toContainText('фактура');
-  await expect(page.locator('.icon-notifications')).toBeEmpty();
+  // element text assertions:
+  await expect(page.getByRole('heading', { name: 'Нова фактура' })).toHaveText('Нова фактура'); // complete text
+  await expect(page.getByRole('heading', { name: 'Нова фактура' })).toContainText('Нова'); // part of the text
+  await expect(page.locator('.universalSearchInputIcon')).toBeEmpty(); // has no text
 
-  // dropdown value assertions
+  // dropdown has value:
   await expect(page.locator('#payment_type')).toHaveValue('cash');
-  // await expect(page.locator('#payment_type')).toHaveValues(['cash', 'bank', 'card']);
-  
-  // element state assertions
+  const locator = page.locator('id=favorite-colors');
+  await locator.selectOption(['R', 'G']);
+  await expect(locator).toHaveValues([/R/, /G/]);
+
+  // element state assertions:
   await expect(page.locator('#client_firmname')).toBeEnabled();
   await expect(page.locator('#inv_number')).toBeDisabled();
   await expect(page.locator('#client_firmname')).toBeEditable();
+  await expect(page.getByRole('checkbox', { name: 'Клиентът е физическо лице' })).not.toBeChecked();
   await expect(page.locator('#client_firmname')).not.toBeFocused();
-  await expect(page.locator('#is_person')).not.toBeChecked();
 
-  // elemnt attribute assertions
+  // element attribute assertions:
   await expect(page.getByRole('button', { name: 'Създай фактурата' })).toHaveId('act_create');
-  await expect(page.getByRole('button', { name: 'Създай фактурата' })).toHaveClass('invform_submit_btn btn_green selenium-submit-invoice');
   await expect(page.getByRole('button', { name: 'Създай фактурата' })).toHaveAttribute('data-additional-text', 'и пренеси плащанията');
+  await expect(page.getByRole('button', { name: 'Създай фактурата' })).toHaveClass('invform_submit_btn btn_green selenium-submit-invoice');
   await expect(page.getByRole('button', { name: 'Създай фактурата' })).toHaveCSS('color', 'rgb(255, 255, 255)');
-  // await expect(page.getByRole('button', { name: 'Създай фактурата' })).toHaveCSS('color', '#fff');
+  await expect(page.getByRole('button', { name: 'Създай фактурата' })).toHaveCSS('color', '#fff');
 
   // Negative match (.not):
-  expect(1).not.toEqual(2)
-  await expect(page.locator('#client_firmname')).not.toBeFocused();
+  expect(1).not.toEqual(0);
+  await expect(page.getByRole('heading', { name: 'Нова фактура' })).not.toContainText('wrong text');
 
-  // soft assertion
-  await expect.soft(page.getByRole('heading', { name: 'Нова фактура' })).toBeVisible();
-  await expect.soft(page.locator('#client_firmname')).toBeEnabled();
-  await expect.soft(page.locator('#payment_type')).toHaveValue('cash');
-  
-  // assert comments
-  expect(1, 'Check if numbers are equal').not.toEqual(2);
-  await expect(page.getByRole('heading', { name: 'Нова фактура' }), 'Check if page heading is visible').toBeVisible();
+  // Soft assertions:
+  // Make a few checks that will not stop the test when failed...
+  await expect.soft(page.getByRole('heading', { name: 'Нова фактура' })).not.toContainText('wrong text');
+  await expect.soft(page.getByRole('checkbox', { name: 'Клиентът е физическо лице' })).not.toBeChecked();
+  await expect.soft(page.locator('#client_firmname')).not.toBeFocused();
+  // ... and continue the test to check more things.
+  await expect(page.getByRole('heading', { name: 'Нова фактура' })).toBeVisible();
+
+  // custom expect messages:
+  expect(56, 'Evaluate if numbers are equal').toBe(56);
+  await expect.soft(page.getByRole('heading', { name: 'Нова фактура' }), 'At correct page').toBeVisible();
 });
