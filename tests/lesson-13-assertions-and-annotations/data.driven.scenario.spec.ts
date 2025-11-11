3cfb2d253fd270aabd24516049eeed31ee837830
VladimirChernev <vchernev@abv.bg>
2025-11-11 22:17:45 +0200

    homework lecture 13 after

--- tests/lesson-13-assertions-and-annotations/data.driven.scenario.spec.ts
+++ tests/lesson-13-assertions-and-annotations/data.driven.scenario.spec.ts
@@ -1,4 +1,27 @@
-// import { test, expect } from '@playwright/test';
+import { test, expect } from '@playwright/test';
 
 // https://playwright.dev/docs/test-parameterize
 
+[
+  { username: 'karamfilovs@gmail.com', password: '111111' },
+  { username: 'secondUsername', password: '2222' },
+  { username: 'thirdUsername', password: '3333' },
+].forEach(({ username, password }) => {
+  test(`Parametrized Scenario with: ${username}`, { tag: '@smoke' }, async ({ page }) => {
+    await test.step('Navigate to Landing page', async () => {
+      await page.goto('/');
+    });
+
+    await test.step('Login into System', async () => {
+      await page.locator('#loginusername').fill(username);
+      await page.locator('#loginpassword').fill(password);
+      await page.locator('#loginsubmit').click();
+      await expect(page).toHaveTitle('Система за фактуриране - QA Ground');
+    });
+
+    await test.step('Navigate to New Invoice page', async () => {
+      await page.getByRole('link', { name: 'Нова Фактура', exact: true }).click();
+      await expect(page.getByRole('heading', { name: 'Нова фактура' })).toBeVisible();
+    });
+  });
+});
