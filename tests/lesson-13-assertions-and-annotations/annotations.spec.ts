3987f8ff04965084b46c23b26d2a8562f55db42e
VladimirChernev <vchernev@abv.bg>
2025-11-11 17:32:46 +0200

    extra souse

--- tests/lesson-13-assertions-and-annotations/annotations.spec.ts
+++ tests/lesson-13-assertions-and-annotations/annotations.spec.ts
@@ -6,9 +6,11 @@ import { test, expect } from '@playwright/test';
   Annotations allow for basic test structure
 */
 
-// test.describe() - creates a test suite
+// test.describe() - creates a group of tests like a test suite. Can have nested subgroups.
 // test.describe.skip - can skip whole test suites
 // test.describe.parallel() - creates a test suite that forces execution of tests within in parallel
+// test.use({ colorScheme: 'dark' }); - set gropup specific scenario configurations
+// tags - at suite and scenario levels
 // test.step() - creates a test step which can contain multiple actions for better reporting
 // tet.only (run only this test, used for development purposes)
 // test.skip() - creates a test that will be skipped (can be added inside a test with condition)
