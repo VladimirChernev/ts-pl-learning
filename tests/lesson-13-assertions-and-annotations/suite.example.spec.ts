3c6f787ecf43af10ef90fc940af05d4c5309e5a3
VladimirChernev <vchernev@abv.bg>
2025-11-12 00:16:01 +0200

    updates suite example

--- tests/lesson-13-assertions-and-annotations/suite.example.spec.ts
+++ tests/lesson-13-assertions-and-annotations/suite.example.spec.ts
@@ -2,58 +2,58 @@ import { test, expect } from '@playwright/test';
 
 // https://playwright.dev/docs/api/class-test#test-describe
 
-test.describe('Test Suite Examples', () => {
-  test('Scenario 1', async ({ page }) => {
-    await test.step('Navigate to Landing page', async () => {
+test.describe('Test Suite Example', () => {
+  test('Scenario #1', async ({ page }) => {
+    await test.step('Navigate to Landing Page', async () => {
       await page.goto('https://st2016.inv.bg/');
+      await expect(page).toHaveTitle('Вход - QA Ground');
     });
 
-    await test.step('Login into System', async () => {
+    await test.step('Login', async () => {
       await page.locator('#loginusername').fill('karamfilovs@gmail.com');
       await page.locator('#loginpassword').fill('111111');
       await page.locator('#loginsubmit').click();
-      await expect(page).toHaveTitle('Система за фактуриране - QA Ground');
     });
 
-    await test.step('Navigate to New Invoice page', async () => {
+    await test.step('Navigate to New Invoice Page', async () => {
       await page.getByRole('link', { name: 'Нова Фактура', exact: true }).click();
-      await expect(page.getByRole('heading', { name: 'Нова фактура' })).toBeVisible();
+      await expect.soft(page.getByRole('heading', { name: 'Нова фактура' }), 'At correct page').toBeVisible();
     });
   });
 
-  test('Scenario 2', async ({ page }) => {
-    await test.step('Navigate to Landing page', async () => {
+  test('Scenario #2', async ({ page }) => {
+    await test.step('Navigate to Landing Page', async () => {
       await page.goto('https://st2016.inv.bg/');
+      await expect(page).toHaveTitle('Вход - QA Ground');
     });
 
-    await test.step('Login into System', async () => {
+    await test.step('Login', async () => {
       await page.locator('#loginusername').fill('karamfilovs@gmail.com');
       await page.locator('#loginpassword').fill('111111');
       await page.locator('#loginsubmit').click();
-      await expect(page).toHaveTitle('Система за фактуриране - QA Ground');
     });
 
-    await test.step('Navigate to New Invoice page', async () => {
+    await test.step('Navigate to New Invoice Page', async () => {
       await page.getByRole('link', { name: 'Нова Фактура', exact: true }).click();
-      await expect(page.getByRole('heading', { name: 'Нова фактура' })).toBeVisible();
+      await expect.soft(page.getByRole('heading', { name: 'Нова фактура' }), 'At correct page').toBeVisible();
     });
   });
 
-  test('Scenario 3', async ({ page }) => {
-    await test.step('Navigate to Landing page', async () => {
+  test('Scenario #3', async ({ page }) => {
+    await test.step('Navigate to Landing Page', async () => {
       await page.goto('https://st2016.inv.bg/');
+      await expect(page).toHaveTitle('Вход - QA Ground');
     });
 
-    await test.step('Login into System', async () => {
+    await test.step('Login', async () => {
       await page.locator('#loginusername').fill('karamfilovs@gmail.com');
       await page.locator('#loginpassword').fill('111111');
       await page.locator('#loginsubmit').click();
-      await expect(page).toHaveTitle('Система за фактуриране - QA Ground');
     });
 
-    await test.step('Navigate to New Invoice page', async () => {
+    await test.step('Navigate to New Invoice Page', async () => {
       await page.getByRole('link', { name: 'Нова Фактура', exact: true }).click();
-      await expect(page.getByRole('heading', { name: 'Нова фактура' })).toBeVisible();
+      await expect.soft(page.getByRole('heading', { name: 'Нова фактура' }), 'At correct page').toBeVisible();
     });
   });
 });
