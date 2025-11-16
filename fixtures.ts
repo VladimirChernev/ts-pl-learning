b95ea782e78d73ce0739ac63199c091970b399aa
ANHristov001 <usmivkata.the.best@gmail.com>
2025-11-16 22:34:36 +0200

    Updating with after new lesson

---
+++ fixtures.ts
@@ -0,0 +1,28 @@
+import { test as base } from '@playwright/test';
+
+export const test = base.extend<{}, { forEachWorker: void }>({
+  forEachWorker: [
+    async ({}, use) => {
+      // This code runs before all the tests in the worker process.
+      const workerIndex = test.info().workerIndex;
+      console.log(`Starting test worker #${workerIndex}`);
+      await use();
+      // This code runs after all the tests in the worker process.
+      console.log(`Stopping test worker #${workerIndex}`);
+    },
+    { scope: 'worker', auto: true }, // automatically start for every worker.
+  ],
+});
+
+// export const test = base.extend<{ forEachTest: void }>({
+//   forEachTest: [
+//     async ({ page }, use) => {
+//       // This code runs before every test.
+//       await page.goto("http://localhost:8000");
+//       await use();
+//       // This code runs after every test.
+//       console.log("Last URL:", page.url());
+//     },
+//     { auto: true },  // automatically start for every test.
+//   ],
+// });
--- tests/lesson-12-locators-and-actions/actions.spec.ts
+++ tests/lesson-12-locators-and-actions/actions.spec.ts
@@ -1,51 +1,83 @@
 import { test } from '@playwright/test';
+import path from 'path';
 
 // documentation: https://playwright.dev/docs/input
 
 test('Actions Examples', async ({ page }) => {
   await page.goto('https://st2016.inv.bg/login');
 
-  // Text Input
-  // await page.locator('#loginusername').fill('username');
+  // Text input (<input>, <textarea> and [contenteditable])
+  await page.locator('#loginusername').fill('Username');
 
-  // Checkbox and Radio buttons
+  // Checkboxes and Radio buttons:
+  await page.locator('.selenium-language-radio-button-EN').check();
   await page.locator('#persistent').check();
-  await page.getByRole('radio', { name: 'English' }).check();
 
-  // Select Option from Dropdown
-  await page.locator('#searchLanguage').selectOption('af');
+  // Selecting option in a dropdown:
+  await page.locator('#searchLanguage').selectOption('Afrikaans'); // both value 'af' or text 'Afrikaans' https://www.wikipedia.org/
+  // Single selection matching the label
+  await page.getByLabel('Choose a color').selectOption({ label: 'Blue' });
+  // Multiple selected items
+  await page.getByLabel('Choose multiple colors').selectOption(['red', 'green', 'blue']);
 
-  // Multiple Option Dropdown Selection
-  await page.locator('Choose multiple colour').selectOption(['red', 'blue', 'green'])
-
-  // Mouse clicks
+  // Mouse clicks:
   // Generic click
-  await page.locator('#loginsubmit').click();
-
+  await page.locator('#persistent').click();
   // Double click
-  await page.locator('.fas.fa-eye').dblclick();
-
+  await page.locator('#passicon').dblclick();
   // Right click
-  await page.locator('#loginusername').click({ button: 'right'});
+  await page.locator('#loginusername').click({ button: 'right' });
+  // Shift + click
+  await page.getByText('Item').click({ modifiers: ['Shift'] });
+  // Ctrl + click on Windows and Linux or Meta + click on macOS:
+  await page.getByText('Item').click({ modifiers: ['ControlOrMeta'] });
+  // Click the top left corner
+  await page.locator('#simplepage').click({ position: { x: 0, y: 0 } });
 
-  // Shift + Click
-  await page.locator('#loginsubmit').click({ modifiers: ['Shift'] });
+  // Focus element:
+  await page.locator('#newpass2').focus();
 
-  // Ctr + click
-  await page.locator('#loginsubmit').click({ modifiers: ['ControlOrMeta'] });
-
-  // Click at specific position (x=0 and y=0 meand top left corner)
-  await page.locator('.login-logo-container--login__img').click({ position: { x: 0, y: 0 } });
-
-  // Focus element
-  await page.locator('.selenium-forgotten-page').focus();
-
-  // Hover element
-  await page.locator('#newpass2').hover();
-
-  // Drag and drop
+  // Drag and drop:
   await page.locator('#newpass2').dragTo(page.locator('#loginusername'));
 
-  // Keyboard input
+  // Keyboard input:
+  // Hit Enter
   await page.locator('#loginpassword').press('Enter');
+  // Key combination
+  await page.getByRole('textbox').press('Control+ArrowRight');
+  await page.getByRole('textbox').press('Shift+ArrowLeft');
+  await page.getByRole('textbox').press('Shift+A');
+  // Press $ sign on keyboard
+  await page.locator('#universalSearchInput').press('$');
+
+  // Hover over element
+  await page.locator('#loginusername').fill('karamfilovs@gmail.com');
+  await page.locator('#loginpassword').fill('111111');
+  await page.getByText('Към списък фактури').hover();
+
+  // Scrolling:
+  // Scrolls automatically so that button is visible
+  await page.getByRole('link', { name: 'Контакти' }).hover();
+  // Scroll the footer into view, forcing an "infinite list" to load more content
+  await page.getByRole('link', { name: 'Контакти' }).scrollIntoViewIfNeeded();
+
+  // Upload files:
+  await page.locator('#loginusername').fill('karamfilovs@gmail.com');
+  await page.locator('#loginpassword').fill('111111');
+  await page.locator('#tabs_documents').click();
+  await page.locator('.newbtn.selenium-new-doc').click();
+  // Select one file
+  await page.locator('.selenium-file-input').setInputFiles(path.join(__dirname, 'xpath_css_dom_ref.pdf'));
+  // Select multiple files
+  await page.getByLabel('Upload files').setInputFiles([path.join(__dirname, 'file1.txt'), path.join(__dirname, 'file2.txt')]);
+  // Select a directory
+  await page.getByLabel('Upload directory').setInputFiles(path.join(__dirname, 'mydir'));
+  // Remove all the selected files
+  await page.getByLabel('Upload file').setInputFiles([]);
+  // Upload buffer from memory
+  await page.getByLabel('Upload file').setInputFiles({
+    name: 'file.txt',
+    mimeType: 'text/plain',
+    buffer: Buffer.from('this is test'),
+  });
 });
