2de9289a668f292e6fd0d32dade57ffc2483cfc6
YordanPopov <jordanpopov190@gmail.com>
2025-11-24 16:11:05 +0200

    add lesson 16 homework

--- tests/homework/lesson.16.spec.ts
+++ tests/homework/lesson.16.spec.ts
@@ -1,20 +1,16 @@
 import { test } from '@tests/steps/step.factory';
 import { Credentials } from '@lib/enums/Credentials';
-import { faker, ru, Faker } from '@faker-js/faker';
+import { expect } from '@playwright/test';
 
 // API Documentation: https://api.inv.bg/v3/swagger-ui/
-
-const customFaker = new Faker({
-  locale: [ru],
-});
 
 [
   {
     username: Credentials.EMAIL,
     password: Credentials.PASSWORD,
-    name: customFaker.commerce.product(),
-    nameEn: faker.commerce.product(),
-    price: faker.number.float({ max: 100, multipleOf: 0.01 }),
+    name: `Портокал-${Date.now()}`,
+    nameEn: `Orange-${Date.now()}`,
+    price: 0.1,
   },
 ].forEach(({ username, password, name, nameEn, price }) => {
   test(
@@ -30,10 +26,18 @@ const customFaker = new Faker({
       const token: string = await apiSteps.obtainAuthToken(username, password);
       // you will need to create new request methods in ApiActions and steps in ApiSteps
       // make an api request to get all items list: "GET /items" and extract "total" number from response
+      const totalItems: number = await apiSteps.getAllItems(token);
+
       const itemId: number = await apiSteps.createItem(token, name, nameEn, price);
       // make an api request to get specific item details: "GET /items/{id}" of the item we created and verify item's name is the one we gave it
+      const createdItemName = await apiSteps.getSpecificItem(token, itemId);
+      expect.soft(createdItemName).toBe(name);
+
       // make an api request to get all items list: "GET /items" again and extract the new "total" number from response
       // compare original "total" items number to new "total" items number expecting for the new one to be incremented by 1
+      const newTotalItems: number = await apiSteps.getAllItems(token);
+      expect.soft(newTotalItems).toBe(totalItems + 1);
+
       await apiSteps.deleteItem(token, itemId);
     },
   );
